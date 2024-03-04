4d7601da12d33755267a98c25c5f48d4ce613c16
Saikal Ulukbekova <saikalulukbekova@Saikals-MacBook-Pro.local>
2024-03-04 07:55:38 +0100

    Add debugtoolbar

--- main/views.py
+++ main/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from goods.models import Categories
 # Create your views here.
 
 def index(request):
+    categories = Categories.objects.all()
     context = {
         "title": "Home",
-        "content": "Welcome to home page"
+        "content": "Welcome to home page",
+        "categories": categories
     }
     return render(request, 'main/index.html', context)
 
