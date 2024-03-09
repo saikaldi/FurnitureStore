ac3e0947254604c2c03cbbf4ba5e823d0e1340c5
Saikal Ulukbekova <saikalulukbekova@Saikals-MacBook-Pro.local>
2024-03-09 09:24:06 +0100

    pagintaion GET parameters

--- goods/urls.py
+++ goods/urls.py
@@ -5,7 +5,7 @@ app_name = 'goods'
 
 urlpatterns = [
     path('<slug:category_slug>/', views.catalog, name='index'),
-    path('<slug:category_slug>/<int:page>/', views.catalog, name='index'),
+    # path('<slug:category_slug>/<int:page>/', views.catalog, name='index'),
     # path('product/', views.product, name = 'product'),
     path('product/<slug:product_slug>/', views.product, name='product')
 ]
