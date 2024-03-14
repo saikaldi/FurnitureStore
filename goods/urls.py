07051fc5795264738f45a4d6e673fdd8424811ab
Saikal Ulukbekova <ukyzsaikal@gmail.com>
2024-03-14 08:42:12 +0100

    Commit

--- goods/urls.py
+++ goods/urls.py
@@ -4,8 +4,7 @@ from goods import views
 app_name = 'goods'
 
 urlpatterns = [
+    path('search/', views.catalog, name = 'search'),
     path('<slug:category_slug>/', views.catalog, name='index'),
-    # path('<slug:category_slug>/<int:page>/', views.catalog, name='index'),
-    # path('product/', views.product, name = 'product'),
     path('product/<slug:product_slug>/', views.product, name='product')
 ]
