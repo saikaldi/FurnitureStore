12d94522091e9c8eeabe3584d78d1a31efb0365f
Saikal Ulukbekova <ukyzsaikal@gmail.com>
2024-03-27 16:11:32 +0100

    Add cart views, included cart templates

--- users/views.py
+++ users/views.py
@@ -16,8 +16,9 @@ def login(request):
                 auth.login(request, user)
                 messages.success(request, f'{username}, You successfully logged in')
 
-                if request.POST.get('next', None):
-                    return HttpResponseRedirect(request.POST .get('next'))
+                redirect_page = request.POST.get('next', None)
+                if redirect_page and redirect_page != reverse('user:logout'):
+                    return HttpResponseRedirect(request.POST.get('next'))
 
                 return HttpResponseRedirect(reverse('main:index'))
     else:
