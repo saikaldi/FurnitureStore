4e89cbab7f9eeb028975afd769c96eb594854eab
Saikal Ulukbekova <ukyzsaikal@gmail.com>
2024-03-28 09:49:16 +0100

    Add Cart remove, commit before ajax

--- carts/views.py
+++ carts/views.py
@@ -22,5 +22,7 @@ def cart_add(request, product_slug):
 
 def cart_change(request, product_slug):
     pass
-def cart_remove(request, product_slug):
-    pass
+def cart_remove(request, cart_id):
+    cart = Cart.objects.get(id=cart_id)
+    cart.delete( )
+    return redirect(request.META.get('HTTP_REFERER'))
